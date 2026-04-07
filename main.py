29c8d5cb56216f5f69ae76ebcb6438e25bf06342
wyze-man <wisdomz.n.truthz@gmail.com>
2026-04-07 16:57:30 -0500

    Update main.py to define missing color variables used in MessageBubble and BigBrainChat classes, and remove unused import statement.

---
+++ main.py
@@ -0,0 +1,10 @@
+# Define existing theme palette
+
+# New color definitions
+PURPLE_DEEP = '#6A1B9A'
+BG_MID = '#F5F5F5'
+GREEN_DIM = '#388E3C'
+BLACK_PURE = '#000000'
+PURPLE_LIGHT = '#D1C4E9'
+PURPLE_MID = '#7E1F4B'
+GREEN_NEON = '#00E676'
